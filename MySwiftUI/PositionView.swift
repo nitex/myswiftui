a072dd92988019f62f9d8b9a11f3828ab28c1761
exo <e@x.o>
2024-05-31 13:54:55 +0300

    2 buttons same width

--- MySwiftUI/PositionView.swift
+++ MySwiftUI/PositionView.swift
@@ -3,20 +3,35 @@
 //  MySwiftUI
 //
 //  Created by EXO on 30.04.2024.
-//
+//  https://github.com/onmyway133/blog/issues/835
 
 import SwiftUI
+
+struct MyActionButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.headline.bold())
+            .foregroundColor(.white)
+            .frame(height: 44)
+            .frame(maxWidth: .infinity)
+            .background(Color.green)
+            .cornerRadius(8)
+    }
+}
 
 struct PositionView: View {
     var body: some View {
         VStack {
             Text("Hello, World!")
                 .padding()
-                .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.blue, lineWidth: 2))
+                .overlay(RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.blue, lineWidth: 2))
                 .shadow(color: .black, radius: 12)
+            
             Text("Hello, World!")
                 .offset(y:14)
                 .padding(.bottom)
+            
             Text("Hello, World!")
                 .padding()
                 .background(Color.orange)
@@ -24,6 +39,19 @@ struct PositionView: View {
                 .background(Color.blue)
                 .colorMultiply(.green)
                 .saturation(3.0)
+            
+            HStack(spacing: 20) {
+                        Button(action: {}) {
+                            Text("Left")
+                        }
+                        .buttonStyle(MyActionButtonStyle())
+
+                        Button(action: {}) {
+                            Text("Right")
+                        }
+                        .buttonStyle(MyActionButtonStyle())
+                    }
+                    .padding(.horizontal, 20)
         }
     }
 }
